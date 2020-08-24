3777369fafc35c84e9ddfcb4dc8011344b5b89c4
Harrison Chung <harrison.y.chung@gmail.com>
2020-08-24 17:11:30 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,10 +1,10 @@
 # Write your #display_rainbow method here.
 def display_rainbow(color)
   colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'}
-    puts "the first color in this format is #{colors[red]}"
-    puts "the second color in this format is #{color[orange]}"
-    puts "the third color in this format is #{color[yellow]}"
-    puts "the fourth color in this format is #{color[green]}"
-    puts "the fifth color in this format is #{color[blue]}"
-    puts "the sixth color in this format is #{color[indigo]}"
-    puts "the third color in this format is #{color[violet]}"
+    puts "the first color in this format is #{colors[R]}"
+    puts "the second color in this format is #{color[O]}"
+    puts "the third color in this format is #{color[Y]}"
+    puts "the fourth color in this format is #{color[G]}"
+    puts "the fifth color in this format is #{color[B]}"
+    puts "the sixth color in this format is #{color[I]}"
+    puts "the third color in this format is #{color[V]}"
