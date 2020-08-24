6af82a2f80b1a1263896b3fcdb728d3dd9cc7cfd
Harrison Chung <harrison.y.chung@gmail.com>
2020-08-24 17:12:25 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,5 +1,5 @@
 # Write your #display_rainbow method here.
-def display_rainbow(color)
+def display_rainbow(colors)
   colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'}
     puts "the first color in this format is #{colors[red]}"
     puts "the second color in this format is #{color[orange]}"
